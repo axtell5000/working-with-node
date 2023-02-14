4a7b3696541240784851d53f4642bc6f6316c7f0
Stephen Axtell <axtell.stephen@gmail.com>
2023-02-14 11:49:43 +0200

    updated form and pages. Started using nodemon

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.get("/currenttime", (request, response) => {
 
 app.get("/", (request, response) => {
   response.send(
-    "<form action='store-user' method='POST'><label>Your Name</label><input type='text' name='username'><button>Submit</button></form>"
+    "<form action='store-user' method='POST'><label>Your Name: </label><input type='text' name='username'><button>Submit</button></form>"
   );
 });
 
@@ -26,6 +26,22 @@ app.post("/store-user", (request, response) => {
   fs.writeFileSync(filePath, JSON.stringify(existingUsers)); // Parsing back to json structure type
 
   response.send("<h1>Username stored!</h1>");
+});
+
+app.get("/users", (request, response) => {
+  const filePath = path.join(__dirname, "data", "users.json"); // setting up path to file
+  const fileData = fs.readFileSync(filePath);
+  const existingUsers = JSON.parse(fileData); // parese to JS object type
+
+  let responseData = "<ul>";
+
+  for (const user of existingUsers) {
+    responseData += "<li>" + user + "</li>";
+  }
+
+  responseData += "</ul>";
+
+  response.send(responseData);
 });
 
 app.listen(3000);
